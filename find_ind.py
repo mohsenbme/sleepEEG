b50593f3265d927304765e9e655a60542538d75c
Mohsen Naji <31356671+mohsenbme@users.noreply.github.com>
2018-06-14 23:36:47 -0700

    Update find_ind.py

--- find_ind.py
+++ find_ind.py
@@ -1,7 +1,8 @@
 def find_ind(x, a):
-    # finds position of 'a' in list x
-    ind=[]
-    for i in range(0,len(x)):
-        if x[i]==a:
-            ind.append(i)
-    return ind[0]
+    # finds position of a in list x
+#     ind=[]
+#     for i in range(0,len(x)):
+#         if round(x[i])==a:
+#             ind.append(i)
+#     return ind[0]
+    return np.argmin(abs(x-a))
